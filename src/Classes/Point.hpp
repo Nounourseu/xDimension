6412fc35e3ae6cf6115919577fc87128322cce2e
Raphaël <raphael.nl.roulin@gmail.com>
2024-03-27 13:14:33 +0100

    Fixed CmakeLists.txt pour pouvoir compiler tt les fichiers cpp

--- src/Classes/Point.hpp
+++ src/Classes/Point.hpp
@@ -1,6 +1,24 @@
 #ifndef POINT_HPP
 #define POINT_HPP
 
-// #include <SFML/Graphics.hpp> 
+#include <SFML/Graphics.hpp> 
+
+class Point {
+public:
+    Point(int x, int y, int size, sf::Color color = sf::Color::White) : position(x, y), color(color) {
+        rectangle.setSize(sf::Vector2f(size, size));
+        rectangle.setFillColor(color);
+        rectangle.setPosition(position.x - size/2, position.y - size/2); // Rectangle centred arround the point
+    }
+
+    void draw(sf::RenderWindow& window) const {
+        window.draw(rectangle);
+    }
+
+private:
+    sf::Vector2i position;
+    sf::Color color;
+    sf::RectangleShape rectangle;
+};
 
 #endif 
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-// J'ai fait la class ici pcq j'arrive pas à include SFML dans Point.hpp
-class Point {
-public:
-    Point(int x, int y, int size, sf::Color color = sf::Color::White) : position(x, y), color(color) {
-        rectangle.setSize(sf::Vector2f(size, size));
-        rectangle.setFillColor(color);
-        rectangle.setPosition(position.x - size/2, position.y - size/2); // Rectangle centred arround the point
-    }
-
-    void draw(sf::RenderWindow& window) const {
-        window.draw(rectangle);
-    }
-
-private:
-    sf::Vector2i position;
-    sf::Color color;
-    sf::RectangleShape rectangle;
-};
-
+#include "Classes/Point.hpp"
 
 int main()
 {
