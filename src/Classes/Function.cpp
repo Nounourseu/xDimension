3307d9cd0e84d98ceb1bae2853baad79ae349720
Raphaël <raphael.nl.roulin@gmail.com>
2024-03-28 09:48:57 +0100

    Small changes

--- src/Classes/Function.cpp
+++ src/Classes/Function.cpp
@@ -7,8 +7,23 @@ Function::Function(){
     m_right_member = nullptr;
 }
 
+//Analyseur sytaxique
 Function::Function(std::string expression){
-    
+    int parenthesis_level = 0;
+    std::string left_expr = "";
+    std::string right_expr = "";
+    bool operator_passed = false;
+    int operator_level = NULL;
+    for (int i = 0; i < expression.size(); i++){
+        if (expression[i] == '('){
+            parenthesis_level ++;
+        }
+        else if (expression[i] == ')')
+        {
+            parenthesis_level --;
+        }
+
+    }
 }
 
 Function::~Function(){
@@ -18,37 +33,49 @@ Function::~Function(){
 
 float Function::image(std::vector<float> values){
     switch(m_operation){
+        float right_val;
         case Addition :
             return m_left_member->image(values) + m_right_member->image(values);
+            break;
         case Substraction :
             return m_left_member->image(values) - m_right_member->image(values);
+            break;
         case Multiplication :
             return m_left_member->image(values) * m_right_member->image(values);
-        case Division :
-            float right_val = m_right_member->image(values);
+            break;
+        case Division : {
+            m_right_member->image(values);
             if (right_val != 0){
                 return m_left_member->image(values) * m_right_member->image(values);
             }
             return __FLT_MAX__; //FIXME: Faire un type d'erreur pris en compte par 
+            break;
+            }   
 
-        case Log :
-            float right_val = m_right_member->image(values);
+        case Log : {
+            m_right_member->image(values);
             if (right_val > 0){
                 return log(m_right_member->image(values));
             }
             throw (1100); //FIXME: Faire un type d'erreur pris en compte par 
+            break;
+            }
 
         case Exponential :
             return exp(m_right_member->image(values));
+            break;
 
         case Power :
             return pow(m_left_member->image(values), m_right_member->image(values));
+            break;
 
         case Constant :
             return m_value;
+            break;
 
         case Variable :
-            values[m_value];
+            return values[m_value];
+            break;
 
     }
 }
