f573439df7e79c6e18fde676c5e09851b58c740c
Nounourseu <44sacha.victor@gmail.com>
2024-04-02 15:32:03 +0200

    Comments

--- src/Classes/Draw2D.cpp
+++ src/Classes/Draw2D.cpp
@@ -11,6 +11,7 @@ Draw2D::Draw2D(std::pair<float, float>& xdef, std::pair<float, float>& ydef, flo
     m_ratio = (ydef.second-ydef.first)/m_winsize.y;
 
     for (int i=0; i<m_winsize.x; i++) {
+        // Il y a encore un peu de maths à faire ici pour que la fonction s'affiche bien en fonction l'ensemble de definition du graphique
         m_points[i].rebuild(i, m_winsize.y - func(i*m_pas-(m_xdef.first*0))/m_ratio, 2, m_color);
     }
 
@@ -20,7 +21,9 @@ Draw2D::~Draw2D() {}
 
 void Draw2D::draw() {
 
+// Les deux segments sont positionnés à x = 0 et y = 0.
     Segment xAxis(  0,
+                    // Jsp pourquoi mais ça marchait pas sans le (int), j'ai passé bcp trop de temps à le debeuguer
                     -(int)(m_winsize.y)*m_ydef.second/(m_ydef.first-m_ydef.second),
                     m_winsize.x,
                     -(int)(m_winsize.y)*m_ydef.second/(m_ydef.first-m_ydef.second)
@@ -41,6 +44,7 @@ void Draw2D::draw() {
 
         m_points[i].draw(*m_window); 
         
+        // On trace un segment pour éviter qu'il n'y ai que des points isolés quand la fct est trop verticale
         if (abs(new_pos.y - prev_pos.y) > 1) {
             Segment seg(prev_pos.x, prev_pos.y, new_pos.x, new_pos.y, m_color);
             seg.draw(*m_window);
