647a1a6314761ad3dd20b17a752cd798546d7bea
Nounourseu <44sacha.victor@gmail.com>
2024-04-19 18:32:09 +0200

    Buttons fixed + click animation

--- src/Classes/UI/Button.hpp
+++ src/Classes/UI/Button.hpp
@@ -6,35 +6,33 @@
 #include "Label.hpp"
 
 class Button {
-    public:
-        static int nbInstances();
-        static Button* getInstance(int index);
+public:
+    static int nbInstances();
+    static Button* getInstance(int index);
 
-        Button(int width, int height, int x, int y, const char text[], void (*m_onclick)(), sf::Color color);
-        ~Button();
+    Button(int width, int height, int x, int y, const char text[], void (*m_onclick)(), sf::Color color);
+    ~Button();
 
-        void (*m_onclick)();
-        void draw(sf::RenderWindow& window);
-        sf::Vector2i getPos();
-        void click(int mousex, int mousey);
+    void draw(sf::RenderWindow& window);
+    sf::Vector2i getPos();
+    void click(int mousex, int mousey);
 
+protected:
+    static int nb_instances;
+    static Button* instances[20]; //Modifier le max si besoin (20)
 
+    sf::Vector2i m_size;
+    sf::Vector2i m_position;
+    char text[20];
+    sf::Color color;  
+    sf::Color calculateTextColor(sf::Color);
+    sf::Vector2i cursor;
 
-    
-    protected:
-        static int nb_instances;
-        static Button* instances[20]; //Modifier le max si besoin (20)
-
-        sf::Vector2i m_size;
-        sf::Vector2i m_position;
-        char text[20];
-        sf::Color color;  
-        sf::Color calculateTextColor(sf::Color);
-        sf::Vector2i cursor;
-
-        // Homemade objects
-        Rectangle rect;   
-        Label label;   
+    // Homemade objects
+    Rectangle rect;   
+    Label label;
+    // Function pointer declaration
+    void (*m_onclick)();
 };
 
 #endif
